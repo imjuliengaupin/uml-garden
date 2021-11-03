950f1ed5fac6188d1213ad0420c37a756b6f2d2b
Julien Gaupin <imjuliengaupin@gmail.com>
2021-11-02 21:20:07 -0400

    class creation

---
+++ uml_garden.py
@@ -0,0 +1,8 @@
+
+class UmlGarden(object):
+
+    def __init__(self, argvs: list[str]) -> None:
+        self.run_uml_garden(argvs)
+
+    def run_uml_garden(self, argvs: list[str]) -> None:
+        pass
