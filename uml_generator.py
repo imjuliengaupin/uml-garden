1638f955fce26d0d5e18d7e8df62c320f7aae388
Julien Gaupin <imjuliengaupin@gmail.com>
2021-11-09 15:12:01 -0500

    feat: file io creation for basic .puml file

--- uml_generator.py
+++ uml_generator.py
@@ -1,8 +1,18 @@
 
+import os
+
+from constants import PLANTUMLS, UML_OPEN, UML_CLOSE
+
+
 class UmlGenerator(object):
 
     def __init__(self, py_files: list[str]) -> None:
         self.py_files: list[str] = py_files
 
     def generate_plantuml_class_figure(self) -> None:
-        pass
+        if not os.path.exists(PLANTUMLS):
+            os.makedirs(PLANTUMLS)
+
+        with open(f"{PLANTUMLS}/uml-garden.puml", 'w') as plantuml_file:
+            plantuml_file.write(f"{UML_OPEN}\n")
+            plantuml_file.write(f"{UML_CLOSE}\n")
