21350efb19573f5fe3947cdc5d85f5e9f43c0196
Julien Gaupin <imjuliengaupin@gmail.com>
2021-11-13 16:38:18 -0500

    func: get_package_name() definition

--- uml_generator.py
+++ uml_generator.py
@@ -11,6 +11,10 @@ class UmlGenerator(object):
     def __init__(self, py_files: list[str]) -> None:
         self.py_files: list[str] = py_files
 
+    def get_package_name(self, index: int) -> str:
+        # return the name of the .py file passed (omitting .py) as an argument to the argvs list to be used as the package name
+        return os.path.basename(self.py_files[index].split('.')[0])
+
     def generate_plantuml_class_figure(self) -> None:
         # by default, create a folder in the project directory to store the output .puml file
         if not os.path.exists(PLANTUMLS):
@@ -39,5 +43,11 @@ class UmlGenerator(object):
             plantuml_file.write(f"{UML_CLOSE}\n")
 
     def write_pre_uml_content(self, plantuml_file: io.TextIOWrapper, index: int) -> None:
+        # extract the name of the .py file passed (omitting .py) as an argument to the argvs list to be used as the package name
+        package_name: str = self.get_package_name(index)
+
         self.class_name = None  # no type hinting recommended
         self.class_variables: dict = {}
+
+        # write out the .py package name to the new .puml file created
+        plantuml_file.write(f"package {package_name} {{\n")
