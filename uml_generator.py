2697428c0cd208cb81d2233b1ab87e7bd4ad95d3
Julien Gaupin <imjuliengaupin@gmail.com>
2021-11-13 16:28:24 -0500

    func: write_pre_uml_content definition

--- uml_generator.py
+++ uml_generator.py
@@ -1,4 +1,5 @@
 
+import io
 import os
 import sys
 
@@ -11,22 +12,32 @@ class UmlGenerator(object):
         self.py_files: list[str] = py_files
 
     def generate_plantuml_class_figure(self) -> None:
-        # by default, create a folder in the project directory to store .puml files
+        # by default, create a folder in the project directory to store the output .puml file
         if not os.path.exists(PLANTUMLS):
             os.makedirs(PLANTUMLS)
 
         # create a new .puml file to write to
         with open(f"{PLANTUMLS}/uml-garden.puml", 'w') as plantuml_file:
 
+            # write out the conventional uml file header to the new .puml file created
             plantuml_file.write(f"{UML_OPEN}\n")
 
-            # for each .py file passed to the program argvs list
+            # for each .py file passed to the program argvs list ...
             for py_file in self.py_files:
-                # validate whether the argument passed is a file or folder
+
+                # ... validate whether the argument passed is a file or folder name
                 if '.' not in py_file:
                     sys.exit("folder argvs are unsupported")
 
-                # capture it's index position in the argvs list
+                # ... capture the arguments index position in the argvs list
                 index: int = self.py_files.index(py_file)
 
+                # ... write out the .py package name to the new .puml file created
+                self.write_pre_uml_content(plantuml_file, index)
+
+            # write out the conventional uml file footer to th new .puml file created
             plantuml_file.write(f"{UML_CLOSE}\n")
+
+    def write_pre_uml_content(self, plantuml_file: io.TextIOWrapper, index: int) -> None:
+        self.class_name = None  # no type hinting recommended
+        self.class_variables: dict = {}
