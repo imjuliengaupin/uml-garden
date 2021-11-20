f745fd9b5ad9879e2a1bc3cfdf1e212afad2330a
Julien Gaupin <imjuliengaupin@gmail.com>
2021-11-19 23:08:25 -0500

    refactor: removal of test code

--- uml_garden.py
+++ uml_garden.py
@@ -1,28 +1,21 @@
 
 import sys
 
-from constants import DEBUG_MODE, LOGGER, MIN_ARGS_REQUIRED
+from constants import MIN_ARGS_REQUIRED
 from uml_generator import UmlGenerator
 
 
 class UmlGarden(object):
 
     def __init__(self, argvs: list[str]) -> None:
-        if DEBUG_MODE:
-            LOGGER.debug(f"{self.run_uml_garden.__doc__}".replace("()", f"(argvs={argvs})"))
         self.run_uml_garden(argvs)
 
     def run_uml_garden(self, argvs: list[str]) -> None:
-        "run_uml_garden()"
-
         # if the minimum # of .py file argvs are provided, remove the default script path argument 
         # so the program is only considering the .py files in the argvs list passed by the user
         argvs.pop(0) if len(argvs) >= MIN_ARGS_REQUIRED else sys.exit(f"insufficient number of argvs provided")
 
         gardener: UmlGenerator = UmlGenerator(argvs)
-
-        if DEBUG_MODE:
-            LOGGER.debug(gardener.generate_plantuml_class_figure.__doc__)
 
         # generate a .puml class inheritance diagram for the .py files passed to the argvs list
         gardener.generate_plantuml_class_figure()
